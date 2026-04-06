d2f296605c01cf7287b5dad8189b0b9789282a83
Nishant8209 <nishant.prabhu@techdenali.com>
2026-04-06 14:43:29 +0530

    added the logs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ const app = import.meta.env.VITE_APP;
 
 const AuthUrl= import.meta.env.VITE_AUTH_URL;
 function App() {
- 
+  console.log('app',app);
+  console.log('AuthUrl',AuthUrl)
   return (
     <>
       <div>{app}</div>
