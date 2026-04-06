1cda183a0435f129ed7d2fef806a2ed2a76ab86e
Nishant8209 <nishant.prabhu@techdenali.com>
2026-04-06 16:25:38 +0530

    added the functionality for the dev

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@
 import './App.css'
 const app = import.meta.env.VITE_APP;
 
-const AuthUrl= import.meta.env.VITE_AUTH_URL;
+const AuthUrl = import.meta.env.VITE_AUTH_URL;
 function App() {
-  console.log('app',app);
-  console.log('AuthUrl',AuthUrl)
+  console.log('app', app);
+  console.log('AuthUrl', AuthUrl)
   return (
     <>
       <div>{app}</div>
       {AuthUrl}
       <div>
         This is the dev branch code
+
+
+        This is the feture branch of 0001
+
       </div>
     </>
   )
